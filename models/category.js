9c2844f4a6cd10f62ebe0600bf3612bc08a5ae51
ColbyCabrera <gdthyispro@gmail.com>
2023-05-22 11:11:59 -0400

    Create category and item models

---
+++ models/category.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+    name: { type: String, required: true, maxLenth: 100 },
+    description: { type: String, required: true },
+});
+
+// Virtual for category URL
+CategorySchema.virtual("url").get(function () {
+    return `/home/category/${this._id}`;
+});
+
+
+// Export model
+module.exports = mongoose.model("Category", CategorySchema);
+
+// Home
+// home/categories
+// home/category/id/items
---
+++ models/item.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true, maxLenth: 100 },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  price: { type: Number, required: true, get: getPrice, set: setPrice },
+  stock: { type: Number, required: true },
+  // image maybe?
+});
+
+function getPrice(num) {
+  return (num / 100).toFixed(2);
+}
+
+function setPrice(num) {
+  return num * 100;
+}
+
+// Virtual for item URL
+ItemSchema.virtual("url").get(function () {
+  return `/home/item/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Item", ItemSchema);
+
+// Home
+// home/categories
+// home/category/id/items
+// home/item/id
