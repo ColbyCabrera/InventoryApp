0e545e83d6857a6bd1befef3ac1932e4e0826367
ColbyCabrera <gdthyispro@gmail.com>
2023-05-26 20:54:25 -0400

    Format code

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,37 +4,37 @@ const asyncHandler = require("express-async-handler");
 // Display home page
 
 exports.index = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: SITE HOME PAGE");
+  res.send("NOT IMPLEMENTED: SITE HOME PAGE");
 });
 
 exports.category_list = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category list");
+  res.send("NOT IMPLEMENTED: category list");
 });
 
 exports.category_detail = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category detail");
+  res.send("NOT IMPLEMENTED: category detail");
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category create get");
+  res.send("NOT IMPLEMENTED: category create get");
 });
 
 exports.category_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category create get");
+  res.send("NOT IMPLEMENTED: category create get");
 });
 
 exports.category_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category update get");
+  res.send("NOT IMPLEMENTED: category update get");
 });
 
 exports.category_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category update post");
+  res.send("NOT IMPLEMENTED: category update post");
 });
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category delete get");
+  res.send("NOT IMPLEMENTED: category delete get");
 });
 
 exports.category_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: category delete post");
+  res.send("NOT IMPLEMENTED: category delete post");
 });
