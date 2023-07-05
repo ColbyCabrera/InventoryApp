f4d52df4c31b4c4b1800bbd4de1175f098789a0b
ColbyCabrera <gdthyispro@gmail.com>
2023-07-05 15:25:39 -0400

    Encrypt passwords using bcrypt

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,6 +5,7 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const session = require("express-session");
 const passport = require("passport");
+const bcrypt = require("bcryptjs");
 
 // Display home page
 
@@ -24,11 +25,17 @@ exports.category_sign_up_get = asyncHandler(async (req, res, next) => {
 
 exports.category_sign_up_post = asyncHandler(async (req, res, next) => {
   try {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      }
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      const result = await user.save();
     });
-    const result = await user.save();
+    
     res.redirect("/");
   } catch (err) {
     return next(err);
